041fc8521ff27fc525b031d69b4490d6cfc7d91b
loyal812 <jh.chan0812@gmail.com>
2024-03-28 22:23:53 -0400

    feat: implement the modal to deploy the endpoint to live

--- src/local_main.py
+++ src/local_main.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     logger.log_example_messages()
 
     run(
-        "src.main:app",
+        "src.local_main:app",
         host="0.0.0.0",
         port=5000,
         reload=True
