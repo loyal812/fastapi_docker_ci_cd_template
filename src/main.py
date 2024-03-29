ad177db7bf59d0badaf562b111f4cd5d4dc5800a
loyal812 <jh.chan0812@gmail.com>
2024-03-28 22:19:43 -0400

    refactor: separated the api endpoint and detailed the project
    Co-authored-by: eureka320 <elias.h0320@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 import os
 import sys
+from uvicorn import run
+from dotenv import load_dotenv
+
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
 
 # Get the current script's directory
 current_script_directory = os.path.dirname(os.path.abspath(__file__))
-
 # Get the project root path
 project_root = os.path.abspath(os.path.join(current_script_directory, os.pardir))
 
@@ -11,33 +15,45 @@ project_root = os.path.abspath(os.path.join(current_script_directory, os.pardir)
 sys.path.append(project_root)
 sys.path.append(current_script_directory)
 
-from typing import List, Optional
+# Import the API endpoints from the endpoints module
+from src.api.endpoints import (
+    docs, 
+)
 
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
-from starlette.responses import RedirectResponse
-from starlette.status import HTTP_201_CREATED
+# Import the CustomLogger class from the logging configuration module
+from src.utils.logging_config import CustomLogger
 
 # Create a FastAPI application
 app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True})
 
 # Configure CORS
 origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:3000",
-    "https://example.com",
-    "https://www.example.com",
+    "*"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get("/")
-async def root():
-    return RedirectResponse(app.docs_url)
+# Include the router for endpoint
+app.include_router(docs.router)
+
+# Run the FastAPI application using Uvicorn server
+if __name__ == "__main__":
+    # Load environment variables from the .env file
+    load_dotenv()
+
+    # Initialize the logger
+    logger = CustomLogger()
+    logger.log_example_messages()
+
+    run(
+        "src.main:app",
+        host="0.0.0.0",
+        port=5000,
+        reload=True
+    )
